c22fbbad09a378f9ae99fab176de6d9985bcaf9d
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-07-19 17:02:52 +0200

    m

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 fetch('https://year-backend.vercel.app/year')
   .then((response) => response.json())
   .then((data) => {
+    console.log(data)
     document.querySelector('#year').textContent = data.year
   })
